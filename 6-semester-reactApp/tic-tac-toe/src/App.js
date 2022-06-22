b273d27ded1278a130c594902f67e1fce26df9b3
badekk <pawel.badysiak@gmail.com>
2022-06-22 10:26:22 +0200

    initial reactCreateApp

---
+++ 6-semester-reactApp/tic-tac-toe/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <button type="button" > </button> <button type="button" > </button> <button type="button" > </button>
+        <br>
+        </br>
+        <button type="button" > </button> <button type="button" > </button> <button type="button" > </button>
+        <br>
+        </br>
+        <button type="button" > </button> <button type="button" > </button> <button type="button" > </button>
+    </div>
+  );
+}
+
+export default App;
